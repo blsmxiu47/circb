55e0a0eaab8ac7b9d0edfba50a432f6cc1827498
Wes Warriner <fridgehistory@gmail.com>
2023-10-02 23:08:03 -0700

    add initial client header for modularized client component

--- include/client.h
+++ include/client.h
@@ -1,11 +1,17 @@
-#ifndef RIRC_COMPONENTS_USER_H
-#define RIRC_COMPONENTS_USER_H
+#ifndef CLIENT_H
+#define CLIENT_H
 
-enum user_err
-{
-	USER_ERR_DUPLICATE = -2,
-	USER_ERR_NOT_FOUND = -1,
-	USER_ERR_NONE
-};
+typedef struct {
+    int socket_fd;
+    char* server_hostname;
+    int server_port;
+    // TODO: additional client-specific data.
+} Client;
+
+Client* init_client(const char* hostname, int port);
+int connect_to_server(Client* client);
+int send_message(Client* client, const char* message);
+char* receive_message(Client* client);
+void close_client(Client* client);
 
 #endif
