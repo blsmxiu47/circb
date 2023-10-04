d7ed151b6c0602aaac25b2268073d2b4d5ce5be1
Wes Warriner <fridgehistory@gmail.com>
2023-10-03 20:22:16 -0700

    begin (wip) implementation of non-blocking server able to accept many clients at once with poll

--- src/components/server.c
+++ src/components/server.c
@@ -1,5 +1,6 @@
 // Sample TCP Server
 #include <netinet/in.h>
+#include <poll.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,24 +10,29 @@
 #include "../../include/server.h"
 
 #define PORT 8080
+#define MAX_CLIENTS 100 // TODO: Likely update this later
 
-void *handle_client(void *client_sock) {
-    int sock = *((int *)client_sock);
-    // The free() function shall cause the space pointed to by ptr to be 
-    // deallocated; that is, made available for further allocation. 
-    // If ptr is a null pointer, no action shall occur.
-    // So, ensures there is memory space for sock in advance
-    free(client_sock);
+void handle_client(int client_fd) {
+	char buffer[1024];
+	// recv() to take in messages from a client, mirrors the method used in the client script
+	int bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
 
-    // TODO: read and write to the client socket, and implement IRC protocol.
+	if (bytes_read <= 0) {
+		// Client disconnected or error
+		close(client_fd);
+		return;
+	}
 
-    close(sock);
-    // from p_thread.h
-    // The pthread_exit() function terminates the calling thread and 
-    // returns a value via retval (NULL here) that (if the thread is joinable) is 
-    // available to another thread in the same process that calls pthread_join(3).
-    // TODO: for the sake of learning, understand what this is doing more thoroughly
-    pthread_exit(NULL);
+	// Null terminate the received data
+	buffer[bytes_read] = '\0';
+	printf("Received: %s\n", buffer);
+
+    	// TODO: add logic to process the message. I believe this is 
+	// ultimately where the magic will happen in terms of proccessing 
+	// commands that the user through the client is sending to the server
+    	// For now, simply just echoing back the message for the sake
+	// of testing
+    	send(client_fd, buffer, bytes_read, 0);
 }
 
 // from server.h
@@ -62,15 +68,81 @@ Server* init_server(char* hostname, int port) {
 }
 
 
+// TODO: move this elsewhere and eventually delete once decisions have been made
+// NOTES on threading/IO: (but currently using poll at the moment)
+	// TODO: I think we're going to be using poll or epoll (and if not, then select. But 
+	// select is limited for modern apps due to 1024 (or 1023?) fds per server.. Check out 
+	// the theoretical O() complexity of each. poll options just seem superior to select(). 
+	// epoll() has additional customization options and functionality over poll(), though is 
+	// linux-specific. epoll() is also technically a group of 3 APIs as oppoosed to just 1 
+	// each for select() and poll(). epoll() also appears to simply have efficiency 
+	// improvements over poll(). The above all really may not matter for my use case, but 
+	// probably better to practice with tools we'd use in real life)
+
+
 void start_server(Server* server) {
-    // init var for server file descriptor
-    int server_fd;
-    // init IPv4 address for server socket
-    struct sockaddr_in address;
-    // Argument for bind(), accept()
-    int addrlen = sizeof(address);
-    // options
-    int opt = 1;
+	// +1 for the main listening socket
+	struct pollfd fds[MAX_CLIENTS + 1];
+	// Start with the main socket
+    	int num_fds = 1;
+
+    	// File descriptor of main listening socket for client connection (index 0)
+	fds[0].fd = server->socket_fd;
+	// data to read
+    	fds[0].events = POLLIN;
+
+	// loop to handle continuous handling of clients 
+	// (in this case the only limit on the number of 
+	// clients is hardware resources and the limit 
+	// set in MAX_CLIENTS
+    	while (1) {
+		// third arg -1 for no timeout
+        	int poll_count = poll(fds, num_fds, -1);
+        	if (poll_count == -1) {
+			perror("poll");
+			break;
+		}
+
+		// Looping over all the file descriptors, up to the limit 
+		// specified in MAX_CLIENTS (+1 for the main listening socket)
+		for (int i = 0; i < num_fds; i++) {
+			// if there is input to be had associated with this file descriptor 
+			if (fds[i].revents & POLLIN) {
+				// if the current file descriptor matches the connection to client
+				// AKA if listening on this socket?
+				if (fds[i].fd == server->socket_fd) {
+					// New client connection
+					struct sockaddr_in client_address;
+					socklen_t client_len = sizeof(client_address);
+					// try to begin accepting connections on this socket
+					int new_client_fd = accept(server->socket_fd, (struct sockaddr*)&client_address, &client_len);
+					if (new_client_fd == -1) {
+						perror("accept");
+						continue;
+					}
+					// refuse connections if this server is hit the limit specified by MAX_CLIENTS
+					if (num_fds >= MAX_CLIENTS) {
+						fprintf(stderr, "Too many clients. Connection refused.\n");
+						close(new_client_fd);
+						continue;
+					}
+
+					// assuming connection is not refused, set the file descriptor of 
+					// the current item in `fds` to the newly created connection
+					fds[num_fds].fd = new_client_fd;
+					// and set the events member to POLLIN (readiness to read)
+                    			fds[num_fds].events = POLLIN;
+					// increment the counter for fds
+                    			num_fds++;
+				} else {
+					// case when existing client sent a message
+                    			handle_client(fds[i].fd);
+				}
+			}
+		}
+	}
+
+   //int opt = 1;
 
     // CREATE
     // Creating socket file descriptor...
@@ -130,27 +202,16 @@ void start_server(Server* server) {
             perror("accept");
             continue;
         }
-	// TODO: I think we're going to be using poll or epoll (and if not, then select. But 
-	// select is limited for modern apps due to 1024 (or 1023?) fds per server.. Check out 
-	// the theoretical O() complexity of each. poll options just seem superior to select(). 
-	// epoll() has additional customization options and functionality over poll(), though is 
-	// linux-specific. epoll() is also technically a group of 3 APIs as oppoosed to just 1 
-	// each for select() and poll(). epoll() also appears to simply have efficiency 
-	// improvements over poll(). The above all really may not matter for my use case, but 
-	// probably better to practice with tools we'd use in real life)
-	// TODO: Annotations
-        pthread_t thread_id;
-	// TODO: Annotations
-        if (pthread_create(&thread_id, NULL, handle_client, (void*) new_sock) < 0) {
-            perror("could not create thread");
-            continue;
-        }
     }
 }
 
 void stop_server(Server* server) {
     // TODO: implement the logic to shut down the server gracefully.
     // This includes closing any open sockets, freeing allocated memory, etc.
+    // The free() function shall cause the space pointed to by ptr to be 
+    // deallocated; that is, made available for further allocation. 
+    // If ptr is a null pointer, no action shall occur.
+    // So, ensures there is memory space for sock in advance
     free(server->hostname);
     // ... Free other resources.
     free(server);
