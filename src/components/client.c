0cbab76ac338a0ef813e89c216dce73e21cf0f82
Wes Warriner <fridgehistory@gmail.com>
2024-01-16 11:54:50 -0700

    fix indentation; remove some superfluous comments

--- src/components/client.c
+++ src/components/client.c
@@ -11,28 +11,26 @@
 #define MAX_MESSAGE_SIZE 1024
 
 Client* init_client(const char* hostname, int port) {
-    	// allocate memory for client object
 	Client* client = malloc(sizeof(Client));
 	// TODO: likely raise to caller
-    	if (!client) {
-        	perror("Failed to allocate memory for client");
-        	exit(EXIT_FAILURE);
-    	}
+	if (!client) {
+		perror("Failed to allocate memory for client");
+		exit(EXIT_FAILURE);
+	}
 	
 	// set client members
-	// hostname comes from argument (specified by user originially)
-    	client->server_hostname = strdup(hostname);
-	// same with port
-    	client->server_port = port;
+	// hostname, port come from arguments (specified by user)
+	client->server_hostname = strdup(hostname);
+	client->server_port = port;
 	// init socket file descriptor to -1 as it is not yet connected
-    	client->socket_fd = -1;
-    	
-    	return client;
+	client->socket_fd = -1;
+	
+	return client;
 }
 
 int connect_to_server(Client* client) {
 	struct sockaddr_in server_address;
-
+	
 	// catch any errors creating a new socket for the client
 	// breaking down the logic: client_fd first set to socket with the following params:
 	//   * domain: AF_INET - IPv4 Internet protocols 
@@ -40,34 +38,28 @@ int connect_to_server(Client* client) {
 	//   * protocol: 0 - Normally only a single protocol exists to support a particular socket type within a given protocol family, in which case protocol can be specified as 0.
 	// On error socket() will return -1, so the below addresses the case in which an error occurred.
 	// On success, socket() will return a file descriptor for the new socket
-	// In terms of why it checks for < 0 as opposed to == -1.. appears to be 
-	// 1. a standard, and 2. technically safer as a general practice since this would 
-	// catch cases where some other error code is returned (though not the case here with socket()
 	if ((client->socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        	perror("Socket creation failed");
-        	return -1;
-    	}
+		perror("Socket creation failed");
+		return -1;
+	}
 	
 	// Set sin_family of our serv_addr struct to AF_INET (see above)
-    	server_address.sin_family = AF_INET;
+	server_address.sin_family = AF_INET;
 	// Network byte order should just refer to big-endian, which is the 
 	// endianness used as standard by TCP/IP
 	// Set sin_port to htons(PORT), so htons(8080) for example. The htons() 
 	// function converts the unsigned short integer hostshort from host byte order 
 	// (which could be big- or little-endian) to network byte order.
-    	server_address.sin_port = htons(client->server_port);
-	// Below,,,
-	//   * af: AF_INET - IPv4 Internet protocols 
+	server_address.sin_port = htons(client->server_port);
+	// Below..
+	//   * af:  AF_INET - IPv4 Internet protocols 
 	//   * src: e.g. localhost, or whatever server_hostname member of client struct ends up being in this case
-	//   * dst: ref to the sin_addr member from serv_addr struct. Assuming this is the address for the server itself. So this is where the converted network address will go.
-	//   Again in this case, if inet_pton() returns -1, then print the error message
-    	// inet_pton returns 1 on success, 0 if `src` does not contain a character
-	// string representing a valid network address in the specified address family,
-	// and -1 if `af` does not contain a valid address family
+	//   * dst: ref to the sin_addr member from serv_addr struct. 
+	//          Assuming this is the address for the server itself. So this is where the converted network address will go.
 	if (inet_pton(AF_INET, client->server_hostname, &server_address.sin_addr) <= 0) {
-        	perror("Invalid address/Address not supported");
-        	return -1;
-    	}
+		perror("Invalid address/Address not supported");
+		return -1;
+	}
 	
 	// Try initiating connection on socket. If error, print message.
 	// int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
@@ -80,11 +72,7 @@ int connect_to_server(Client* client) {
 	//   I know, this is a kind of "polymorphism" : the bind() function pretends to take a struct sockaddr *, 
 	//   but in fact, it will assume that the appropriate type of structure is passed in; i. e. one that 
 	//   corresponds to the type of socket you give it as the first argument."
-	//   * addrlen: size of addr. (Why is this needed? Do you ever set this to anything other than the 
-	//   size of the prev arg? Probably only in cases where for whatever reason the second arg for 
-	//   address is more than just the address, like a struct containing address struct as a member 
-	//   inside it, with additional members that should not be counted in sizeof, though atm not 
-	//   sure of a good example of this)
+	//   * addrlen: size of addr
 	if (connect(client->socket_fd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
 		perror("Connection failed");
 		return -1;
@@ -115,12 +103,12 @@ char* receive_message(Client* client) {
 	// Similar to read()ing to buffer for now while using TCP communication 
 	// in a blocking manner, but recv() makes it clearer we are working with 
 	// sockets and also allows flags and flexibility in case we need it.
-    	// ssize_t recv(int sockfd, void buf[.len], size_t len, int flags);
+	// ssize_t recv(int sockfd, void buf[.len], size_t len, int flags);
 	int bytes_received = recv(client->socket_fd, buffer, sizeof(buffer) - 1, 0);
-    	if (bytes_received < 0) {
-        	perror("Receive failed");
-        	return NULL;
-    	}
+	if (bytes_received < 0) {
+		perror("Receive failed");
+		return NULL;
+	}
 
 	return buffer;
 }
@@ -137,7 +125,7 @@ void close_client(Client* client) {
 
 int main (int argc, char* argv[]) {
 	// TODO: not necessarily assuming command line args are given for the 
-	// foirst time I think this will be okay but we should be prepped 
+	// first time I think this will be okay but we should be prepped 
 	// for the future when making this more flecible for other use cases
 	const char* hostname = argv[1];
 	// port as well from arguments, though this should also be parsed from config file no?
@@ -161,11 +149,11 @@ int main (int argc, char* argv[]) {
 	// Using `poll()`... init fds with length of 2
 	// The set of file descriptors to be monitored is specified in the 
 	// fds argument, which is an array of structures of the following form:
-        //   struct pollfd {
-        //       int   fd;         /* file descriptor */
-        //       short events;     /* requested events */
-        //       short revents;    /* returned events */
-        //   };
+	//   struct pollfd {
+	//       int   fd;         /* file descriptor */
+	//       short events;     /* requested events */
+	//       short revents;    /* returned events */
+	//   };
 	struct pollfd fds[2];
 	// standard input (user input)
 	fds[0].fd = STDIN_FILENO;
@@ -197,28 +185,28 @@ int main (int argc, char* argv[]) {
 			fgets(message, MAX_MESSAGE_SIZE, stdin);
 
 			// Check if user wants to disconnect
-            		if (strncmp(message, "/disconnect", 11) == 0) {
-                		should_run = 0;
-                		break;
-            		}
+			if (strncmp(message, "/disconnect", 11) == 0) {
+				should_run = 0;
+				break;
+			}
 			
 			// actually try sending the message specified by user
-            		send_message(client, message);
+			send_message(client, message);
 		}
 
 		// If there is activity from the server (via socket, second file descriptor)
 		if (fds[1].revents & POLLIN) {
 			// get response from client (using `recv()`)
 			char* response = receive_message(client);
-            		if (response) {
-				// if non-null, print so that user can see it
-                		printf("Server: %s\n", response);
-            		} else {
-				// otherwise, notify that server is not connected via socket and break
-                		printf("Server disconnected.\n");
-                		should_run = 0;
-                		break;
-            		}
+				if (response) {
+					// if non-null, print so that user can see it
+					printf("Server: %s\n", response);
+				} else {
+					// otherwise, notify that server is not connected via socket and break
+					printf("Server disconnected.\n");
+					should_run = 0;
+					break;
+				}
 		}
 	}
 	// Sample communication for testing..
@@ -226,7 +214,7 @@ int main (int argc, char* argv[]) {
 	//send_message(client, "Hello, Server!");
 	// TODO: failing in the recv() call here atm, returns null response
 	// char* response = receive_message(client);
-    	// printf("Received from server: %s\n", response);
+	// printf("Received from server: %s\n", response);
 	// close_client(client);
 
 	return EXIT_SUCCESS;
