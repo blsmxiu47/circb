d942294d047279d8e6beef22644959839532b47c
Wes Warriner <fridgehistory@gmail.com>
2024-02-19 11:02:32 -0700

    adding scripts I've initialized over time to bring repo up to date for future development

---
+++ src/state.c
@@ -0,0 +1,9 @@
+// #include "../include/state.h"
+
+// void init_state(State* state) {
+//     state->server = NULL;
+
+//     state->client = NULL;
+//     state->config = init_config();
+//     state->should_run = 1;
+// }
